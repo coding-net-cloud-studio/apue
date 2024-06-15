b5404d7a6d6a4a3f5f9f2f97a4c666194f2ea147
mymy20220326 <mymy20220326@outlook.com>
2024-06-15 23:23:35 +0800

    2024_06_15_0580-wmtag_memo_尝试格式化文件_2_个

--- environ/cmd2.c
+++ environ/cmd2.c
@@ -1,14 +1,13 @@
 #include "apue.h"
 #include <setjmp.h>
 
-#define	TOK_ADD	   5
+#define TOK_ADD 5
 
-jmp_buf	jmpbuffer;
+jmp_buf jmpbuffer;
 
-int
-main(void)
+int main(void)
 {
-	char	line[MAXLINE];
+	char line[MAXLINE];
 
 	if (setjmp(jmpbuffer) != 0)
 		printf("error");
@@ -17,15 +16,12 @@ main(void)
 	exit(0);
 }
 
- . . .
-
-void
-cmd_add(void)
+void cmd_add(void)
 {
-	int		token;
+	int token;
 
 	token = get_token();
-	if (token < 0)		/* an error has occurred */
+	if (token < 0) /* an error has occurred */
 		longjmp(jmpbuffer, 1);
 	/* rest of processing for this command */
 }
